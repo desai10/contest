b2b68385236016689f4b0bdc3faa6400f1dc007f
Sriram Desai <isriramdesai@gmail.com>
2019-09-22 21:19:20 +0530

    first commit

---
+++ src/main/java/com/ai/contest/ContestApplication.java
@@ -0,0 +1,13 @@
+package com.ai.contest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestApplication.class, args);
+	}
+
+}
